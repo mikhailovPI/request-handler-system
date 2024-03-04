84586658de2df74c453631d48f36f7fbca1dd3a2
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2024-03-04 23:14:39 +0300

    commit init

---
+++ src/main/java/ru/mikhailov/requesthandlersystem/RequestHandlerSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.requesthandlersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHandlerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestHandlerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mikhailov/requesthandlersystem/RequestHandlerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.requesthandlersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestHandlerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
